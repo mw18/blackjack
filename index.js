79c789d5f6f4cff41af1b8dfe2ac785bc2740f26
mw18 <67554339+mw18@users.noreply.github.com>
2021-07-19 15:50:09 -0500

    write logical opertator for newCard() write object to store player data

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+// let player = {
 
+//     name: "Melissa",
+//     chips: "145"
+// }
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -7,6 +11,11 @@ let message = " "
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+
+// playerEl.textContent = player.name + ": $" + player.chips
+
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1
@@ -24,8 +33,8 @@ function startGame() {
     isAlive = true
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
-    let cards = [firstCard, secondCard]
-    let sum = firstCard + secondCard
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
 
     renderGame()
 }
@@ -53,13 +62,14 @@ function renderGame() {
 
 
 function newCard() {
-    console.log("drawing a new card from the deck")
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
 
-    renderGame()
 }
 
 
