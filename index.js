8403a1cb98fface0ce118aef3b595c8d2127dd47
mw18 <67554339+mw18@users.noreply.github.com>
2021-07-19 13:10:01 -0500

    write getRandomCard function and assign values in startGame function

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [ firstCard, secondCard]
-let sum = firstCard + secondCard
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
 let isAlive = true
 let message = " "
@@ -9,15 +8,32 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    let cards = [firstCard, secondCard]
+    let sum = firstCard + secondCard
+
     renderGame()
 }
 
 
 function renderGame() {
     cardsEl.textContent = "Cards: "
-    for ( let i = 0; i < cards.length; i++){
+    for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
 
@@ -39,7 +55,7 @@ function renderGame() {
 function newCard() {
     console.log("drawing a new card from the deck")
 
-    let card = 6
+    let card = getRandomCard()
     sum += card
     cards.push(card)
 
